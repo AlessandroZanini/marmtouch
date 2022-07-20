db2f88f2eac020d93ac459010866157e9116d12e
Raymond Wong <raymond.wong@uwo.ca>
2022-07-20 15:42:15 -0400

    implement-item-preview

--- marmtouch/scripts/__init__.py
+++ marmtouch/scripts/__init__.py
@@ -3,6 +3,7 @@ import click
 from marmtouch import __version__
 from marmtouch.scripts.launcher import launch
 from marmtouch.scripts.make_shortcut import make_shortcut
+from marmtouch.scripts.preview_items import preview_items
 from marmtouch.scripts.run import run
 from marmtouch.scripts.transfer_files import transfer_files
 
@@ -17,6 +18,7 @@ marmtouch.add_command(run)
 marmtouch.add_command(make_shortcut)
 marmtouch.add_command(transfer_files)
 marmtouch.add_command(launch)
+marmtouch.add_command(preview_items)
 
 if __name__ == "__main__":
     marmtouch()
